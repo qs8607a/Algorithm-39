e8b85fbce4c2319ea2f20e0e1f0fbf2651dd49dd
llq <llq17501@gmail.com>
2012-10-12 11:00:52 +0800

    complete 141

--- Euler/test.py
+++ Euler/test.py
@@ -5,11 +5,26 @@
 """
 from timeit import Timer
 from cProfile import run
-from part3.p133 import main
 
+
+from itertools import count
+def f():
+    d = 312
+    for b in range(2, 47):
+        for a in range(1, b):
+            for c in count(1):
+                x = a * a * c + a * c * c * b * b * b
+                if x == d * d:
+                    return a, b, c
+                elif x > d * d:
+                    break
 
 def test():
-    main()
-t=Timer("test()","from __main__ import test")
-print(t.timeit(1))
-run('test()')
+    print f()
+
+if __name__ == "__main__":
+    t=Timer("test()","from __main__ import test")
+    print(t.timeit(1))
+    run('test()')
+
+
