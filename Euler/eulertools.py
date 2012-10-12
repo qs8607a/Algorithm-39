6c5547dce6c3f6ea4a6026293255006171b5c8b7
llq <llq17501@gmail.com>
2012-10-12 18:19:54 +0800

    complete 144 & uncomplete 146

--- Euler/eulertools.py
+++ Euler/eulertools.py
@@ -3,6 +3,24 @@ import time
 from itertools import cycle
 
 
+def is_prime(n):
+    if n==2:
+        return True
+    if n%2==0:
+        return False
+    i=3
+    while i*i<=n:
+        if n%i==0:
+            return False
+        i=i+2
+    return True
+
+
+
+def is_primie_fast(n):
+    pass
+
+
 
 def is_square(n):
     """
---
+++ Euler/part3/p146.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from eulertools import is_prime
+
+def f(n):
+    """
+    x % 21 = 4, 10, 11, 17
+    >>> sum(f(1000000))
+    1242490
+    >>> sum(f(100))
+    10
+    """
+    for j in xrange((n / 210) + 1):
+        tmp = 210 * j
+        for i in (tmp + 10, tmp + 80, tmp + 130, tmp + 200):
+            if i % 13 not in (1, 3, 4, 9, 10, 12):
+                continue
+            if i< n and all(map(lambda x:is_prime(i * i + x), (1, 3, 7, 9, 13, 27))):
+                yield i
