7ddeb3b855ce0e8d9df7f57d45c96f98793e810e
llq <llq17501@gmail.com>
2012-10-25 18:48:51 +0800

    add 150

--- Euler/eulertools.py
+++ Euler/eulertools.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import time
 from itertools import cycle
+from functools import partial
 from random import randint
 
 
@@ -135,3 +136,31 @@ def skip(iter_item, n):
         next(iter_item)
     for item in iter_item:
         yield item
+
+
+
+class memoized(object):
+   """Decorator that caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned, and
+   not re-evaluated.
+   """
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      try:
+         return self.cache[args]
+      except KeyError:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+      except TypeError:
+         # uncachable -- for instance, passing a list as an argument.
+         # Better to not cache than to blow up entirely.
+         return self.func(*args)
+   def __repr__(self):
+      """Return the function's docstring."""
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      """Support instance methods."""
+      return partial(self.__call__, obj)
---
+++ Euler/part3/p150.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from itertools import count
+from eulertools import memoized
+
+def max_sub_triangle(triangle):
+    """
+    >>> max_sub_triangle([15, -14, -7, 20, -13, -5, -3, 8, 23, -26, 1, -4, -5, -18, 5, -16, 31, 2, 9, 28, 3])
+    -42
+    """
+    for i, v in enumerate(count_triangle_number(), 1):
+        if triangle_number(v) == len(triangle):
+            n = i + 1
+            break
+        elif triangle_number(v) > len(triangle):
+            raise Exception()
+
+    result = 0
+    for deep, v in enumerate(count_triangle_number(), 1):
+        for j in range(deep):
+            print "deep: ", deep
+            i = v - deep + j + 1
+            for high in range(2, n - deep + 1):
+                #TODO
+                print "high: ", high
+                tmp = sum(sum(triangle[triangle_number(deep + m) + j: triangle_number(deep + m) + j + m + 1]) for m in range(high))
+                print [triangle[triangle_number(deep + m) + j: triangle_number(deep + m) + j + m + 1] for m in range(high)]
+                result = min(result, tmp)
+        if deep >= n:
+            break
+
+    return result
+
+
+triangle_number = memoized(lambda x:(x * (x + 1)) / 2)
+
+
+def count_triangle_number():
+    for i in count(1):
+        yield triangle_number(i)
+
