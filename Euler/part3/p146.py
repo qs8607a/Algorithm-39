b70de12335f960d66ee83d166f8e90f0d3224756
llq <llq17501@gmail.com>
2012-10-26 11:31:25 +0800

    done 146

--- Euler/part3/p146.py
+++ Euler/part3/p146.py
@@ -2,28 +2,29 @@
 #-*- coding:utf-8 -*-
 
 import operator
-from eulertools import is_prime_fast as is_prime
+from eulertools import is_prime_fast as is_prime, primes
 
 ADD_List = (1, 3, 7, 9, 13, 27)
 
 def f(n):
     """
-    x % 21 = 4, 10, 11, 17
     #11914460
     >>> sum(f(15 * (10 ** 7)))
-    821107610
+    676333270
     >>> sum(f(2000000))
     1242490
     >>> sum(f(100))
     10
     """
-    steps = list(get_steps((3, 7, 13)))
-    for k in xrange(0, n, 3 * 7 * 13 * 10):
+    ps = list(primes(18))
+    steps = list(get_steps(ps))
+    for k in xrange(0, n, reduce(operator.mul, ps)):
         for j in steps:
             i = j + k
             if i >= n:
                 break
-            if i< n and all(map(lambda x:is_prime(i * i + x), ADD_List)):
+            sq_i = i * i
+            if i< n and all(map(lambda x:is_prime(sq_i + x), ADD_List)) and all(map(lambda x:not is_prime(sq_i + x), [11, 17, 19, 21, 23])):
                 yield i
 
 
@@ -31,11 +32,11 @@ def get_steps(ps):
     r = {}
     for p in ps:
         s = set(map(lambda x: (x *(p - 1))% p, ADD_List))
-        r[p] = [i for i in range(p) if (i * 10 * i * 10) % p not in s]
+        r[p] = [i for i in range(p) if (i *  i ) % p not in s]
 
     for i in range(reduce(operator.mul, ps)):
         if all(i %p in r[p] for p in ps):
-            yield i * 10
+            yield i
 
 if __name__ == "__main__":
     print list(f(100))
