f651dd0e5d74221b619b7170ff9010c5afc3c024
llq <llq17501@gmail.com>
2012-10-26 15:11:45 +0800

    update 147

--- Euler/part3/p147.py
+++ Euler/part3/p147.py
@@ -5,16 +5,31 @@ from itertools import product
 
 def f(m, n):
     """
+    >>> f(47, 43)
+    1
+    >>> f(1, 1)
+    1
+    >>> f(2, 1)
+    4
+    >>> f(3, 1)
+    8
+    >>> f(2, 2)
+    18
     >>> f(3, 2)
     37
     """
     result = 0
-    a =  g(m , n)
-    b = 19
-    for i, j in product(range(2 * m), range(2 * n)):
-        _ = (0, i + j), (2 * m, i + 2 * n - j)
-        _ = (2 * m - i, 2 * n + j)
-    return a + b
+    for a, b in product(range(1, max(m, n) +1), range(1, max(n, m) + 1)):
+        tmp = 0
+        for i in range(a, 2 * n - b + 1):
+            if i % 2 == 1:
+                t = (2 * m + 1 - (a + b )) / 2
+            else:
+                t = m - (a + b - 1) / 2
+            tmp += t
+        result += tmp
+    result += g(m, n)
+    return result
 
 def g(m, n):
     return sum((m - i) * (j + 1) for i in range(m) for j in range(n))
