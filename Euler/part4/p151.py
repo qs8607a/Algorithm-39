3559749d52b45db0ed34ef8a3dd0fa299c3a42b8
llq <llq17501@gmail.com>
2012-10-26 18:42:11 +0800

    fix 151

--- Euler/part4/p151.py
+++ Euler/part4/p151.py
@@ -4,16 +4,19 @@
 from collections import defaultdict
 from fractions import Fraction
 
-def f():
-    statuses = {(1, 1, 1, 1) : Fraction(1, 1)}
+def f(n):
+    """
+    >>> float(f(4))
+    0.464398
+    """
+    statuses = {(0, tuple(1 for i in range(n))) : Fraction(1, 1)}
     result = 0
-    for _ in range(14):
+    for _ in range(2 ** n - 2):
         new_statuses = defaultdict(Fraction)
         for status, p in statuses.iteritems():
             _sum = sum(status)
             if _sum == 1:
                 result += p
-                print status, p
             for i, v in enumerate(status):
                 if v != 0:
                     new_statuses[cut_in_half(status, i)] += p * Fraction(v, _sum)
@@ -21,13 +24,7 @@ def f():
 
     return result
 
-def mul(a, b):
-    return a[0] * b[0], a[1] * b[1]
-
-def add(a, b):
-    return (a[0] * b[1] + a[1] * b[0]) , a[1] * b[1]
 
 def cut_in_half(status, use):
     return tuple(v if i < use else v - 1 if i == use else v + 1 for i, v in enumerate(status))
 
-print float(f())
