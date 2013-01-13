f3d1ad2860992745b7614ba1185500d437dc2984
llq <llq17501@gmail.com>
2013-01-13 22:51:43 +0800

    166

--- Euler/part4/p166.py
+++ Euler/part4/p166.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 
 from functools import partial
+from itertools import product
 from operator import add
 
 L = [
@@ -24,7 +25,25 @@ L = [
     ]
 
 def f():
-    pass
+    """
+        >>> f()
+        7130034
+    """
+    re = 0
+    for a, b, c, d, e, f, g, h in product(*[range(10) for i in range(8)]):
+        lst = [
+                a + b + c + d - e - f - g,
+                a - d + e - g + h,
+                b + c + 2 * d - e -f - h,
+                f + g - h,
+                b + c + d - e - h,
+                c + 2 * d - e - f + g - h,
+                a - c - d + e + f - g + h,
+                0 - d + e + h,
+            ]
+        if all(map(lambda x:0<=x<=9, lst)):
+            re += 1
+    return re
 
 def g(op, args):
     return reduce(partial(map, op), args)
@@ -35,3 +54,7 @@ def check():
         print g(add, [L[i + j * 4] for j in range(4)])
     print g(add, [L[0], L[5], L[10], L[15]])
     print g(add, [L[3], L[6], L[9], L[12]])
+
+
+if __name__ == "__main__":
+    print f()
