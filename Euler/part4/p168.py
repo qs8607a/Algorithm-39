9a93945d78e7d7e41a1e09b8276f051835a4f356
llq <llq17501@gmail.com>
2013-01-24 18:38:27 +0800

    add 168

---
+++ Euler/part4/p168.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+def f():
+    result = 0
+    count = 0
+    for a in range(1, 10):
+        for b in range(1, 10):
+            p = 0
+            re = v = a
+            for i in range(99):
+                l_v = v
+                p, v = divmod(v * b + p, 10)
+                if v == a and p == 0 and l_v != 0 and re > 10:
+                    print re, b
+                    result += re
+                    count += 1
+                re += v * 10 ** (i + 1)
+    print count
+    return str(result)
+
